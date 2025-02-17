856591e19d8b8961348da8090a52b1b690e3a5dd
filix <florindozec@gmail.com>
2025-02-17 21:15:37 +0100

    creazione model e init API microService access

--- MicroService/AccessService/src/main/java/org/ac/API/LoginRestFulAPI.java
+++ MicroService/AccessService/src/main/java/org/ac/API/LoginRestFulAPI.java
@@ -3,16 +3,16 @@ package org.ac.API;
 import jakarta.ws.rs.core.MediaType;
 import org.ac.API.Interfacce.LoginAPI;
 import jakarta.ws.rs.*;
+import org.md.Utente.Utente;
 
-
-@Path("")
+@Path("makeLogin")
 public class LoginRestFulAPI implements LoginAPI {
 
     //LoginService login_access = new LoginService();
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String makeLogin() {
-        return "Hello, World!";
+    public String makeLogin(Utente utente) {
+        return "";
     }
 }
