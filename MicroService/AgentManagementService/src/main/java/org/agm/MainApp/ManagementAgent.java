1a4c41d453ec4942554cf57ec7d04faa73d7b28e
Domenico Gagliotti <domenicogagliotti3@gmail.com>
2025-02-22 01:45:49 +0100

    validetor in gestione Agent

--- MicroService/AgentManagementService/src/main/java/org/agm/MainApp/ManagementAgent.java
+++ MicroService/AgentManagementService/src/main/java/org/agm/MainApp/ManagementAgent.java
@@ -1,13 +1,43 @@
 package org.agm.MainApp;
 
 import org.agm.MainApp.Interfacce.ManagmentAgentService;
+import org.agm.Validetor.Validate;
+import org.dao.Interfacce.AgentDAO;
+import org.dao.postgre.AgentPostgreDAO;
+import org.exc.DietiEstateException;
 import org.md.Utente.Agent;
 
 public class ManagementAgent implements ManagmentAgentService {
 
+    private AgentDAO agentDAO = new AgentPostgreDAO();
+
     @Override
     public String addAgent(Agent agent) {
-        return null;
+        try{
+            agentDAO.isUserAbsent(agent);
+        }catch (DietiEstateException e){
+            return e.getMessage();
+        }
+
+        //TODO implementa
+        Validate validaitor = Validate.getInstance();
+
+
+        try{
+            validaitor.validateName(agent.getNome());
+            validaitor.validateCognome(agent.getCognome());
+            validaitor.validateEmail(agent.getEmail());
+            validaitor.validatePassword(agent.getPassword());
+
+            agent.createUser(agent);
+
+            //TODO FARE un oggetto per messaggio di buon fine
+            return "{\"code\": 0, \"message\": \"success of action admin create\"}";
+
+
+        }catch(DietiEstateException e){
+            return e.getMessage();
+        }
     }
 
     @Override
