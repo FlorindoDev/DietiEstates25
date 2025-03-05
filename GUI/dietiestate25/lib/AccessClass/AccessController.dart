0d11299f79351574be61f4f75c627da56d23c095
filix <florindozec@gmail.com>
2025-03-05 18:32:59 +0100

    scroll con la testiera alzata

--- GUI/dietiestate25/lib/AccessClass/AccessController.dart
+++ GUI/dietiestate25/lib/AccessClass/AccessController.dart
@@ -16,8 +16,8 @@ import 'package:dietiestate25/AccessClass/SingUpWindow.dart';
 
 class AccessController {
   //static final url = Uri.parse("http://api.florindodev.site/makeLogin");
-  static final url = Uri.parse("http://127.0.0.1:7001/login/makeLogin");
-  //static final url = Uri.parse("http://10.0.2.2:7001/makeLogin");
+  //static final url = Uri.parse("http://127.0.0.1:7001/login/makeLogin");
+  static final url = Uri.parse("http://10.0.2.2:7001/login/makeLogin");
 
   static String token = "";
   static Validator valida = Validate();
@@ -70,43 +70,52 @@ class AccessController {
 
     richiestaLogin(utente).then((risultato) {
       //risultato = json.decode(risultato);
-      if (risultato == null){
+      if (risultato == null) {
         MyApp.mostraPopUpInformativo(context, "Attenzione", "Servizio non attualmente disponibile, prova tra qualche minuto");
         return false;
-      }else if (risultato['code'] == 1) {
+      } else if (risultato['code'] == 1) {
         MyApp.mostraPopUpInformativo(context, "Attenzione", risultato['message']);
         return false;
-      
       } else if (risultato['code'] == 0) {
         print('1');
-        scriviJWTInFile(risultato['message']);print('12');
-        MyApp.mostraPopUpInformativo(context, "Complimenti", "Login Effettuato!");print('13');
+        scriviJWTInFile(risultato['message']);
+        print('12');
+        MyApp.mostraPopUpInformativo(context, "Complimenti", "Login Effettuato!");
+        print('13');
         Navigator.of(context).pop();
         Navigator.of(context).pushNamed(RouteWindows.homeWindow);
         return true;
       }
     });
-  
-    
-    
+
     print("finito prova");
     return false;
   }
 
-  static Future<void> scriviJWTInFile(jwt) async {print('2');
-    final directory = await getApplicationDocumentsDirectory(); print('2');
-    final file = File('${directory.path}/dietiEstate25.json');print('3');
-    if ((await file.exists())) {print('4');
-      String content = await file.readAsString();print('5');
-      dynamic json = jsonDecode(content); print('6');
-      print(jsonEncode(json));print('7');
-      json['JWT'] = jwt;print('8');
-      print(jsonEncode(json));print('9');
-      await file.writeAsString(jsonEncode(json));print('10');
-
-    }else{}print('11');
-      await file.writeAsString("{ \"JWT\":\"$jwt\" }");print('12');
-    
+  static Future<void> scriviJWTInFile(jwt) async {
+    print('2');
+    final directory = await getApplicationDocumentsDirectory();
+    print('2');
+    final file = File('${directory.path}/dietiEstate25.json');
+    print('3');
+    if ((await file.exists())) {
+      print('4');
+      String content = await file.readAsString();
+      print('5');
+      dynamic json = jsonDecode(content);
+      print('6');
+      print(jsonEncode(json));
+      print('7');
+      json['JWT'] = jwt;
+      print('8');
+      print(jsonEncode(json));
+      print('9');
+      await file.writeAsString(jsonEncode(json));
+      print('10');
+    } else {}
+    print('11');
+    await file.writeAsString("{ \"JWT\":\"$jwt\" }");
+    print('12');
   }
 
   static void toSignUp(Utente utente) {
